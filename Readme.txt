4e33f57bf851076f6f99e9bb87a4ffb4ec413dab
RITIK KUMAR <ritik805131@gmail.com>
2022-09-03 12:43:30 +0530

    commit

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Netflix clone web application using reactjs
+https://netflix-clone-e0e30.firebaseapp.com/
---
+++ src/Request.js
@@ -0,0 +1,7 @@
+const API_KEY = "8afb4aa3d41fjhjhbjhgg5144caeccc3dd28";
+
+const userrequests = {
+    
+}
+
+export default userrequests
